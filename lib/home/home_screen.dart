0153135134b79955370c160f69301ee510706d77
Dimitrina Toleva <dimitrina.toleva@trading212.com>
2022-05-17 17:17:44 +0300

    History screen

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:faculty_demo_app/history/history_screen.dart';
 import 'package:faculty_demo_app/home/home_element.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -62,20 +63,65 @@ class HomeScreen extends StatelessWidget {
 
               return Padding(
                 padding: const EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    SvgPicture.asset(
-                      item.imagePath,
-                      height: 50,
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    Text(
-                      item.title,
-                      textAlign: TextAlign.center,
-                    )
-                  ],
+                child: InkWell(
+                  onTap: () {
+                    switch (item) {
+                      case HomeElement.aboutUs:
+                        // TODO: Handle this case.
+                        break;
+                      case HomeElement.history:
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => HistoryScreen()));
+                        break;
+                      case HomeElement.news:
+                        // TODO: Handle this case.
+                        break;
+                      case HomeElement.candidateStudents:
+                        // TODO: Handle this case.
+                        break;
+                      case HomeElement.education:
+                        // TODO: Handle this case.
+                        break;
+                      case HomeElement.careerCenter:
+                        // TODO: Handle this case.
+                        break;
+                      case HomeElement.library:
+                        // TODO: Handle this case.
+                        break;
+                      case HomeElement.museums:
+                        // TODO: Handle this case.
+                        break;
+                      case HomeElement.bases:
+                        // TODO: Handle this case.
+                        break;
+                      case HomeElement.fss:
+                        // TODO: Handle this case.
+                        break;
+                      case HomeElement.baseReservation:
+                        // TODO: Handle this case.
+                        break;
+                      case HomeElement.faq:
+                        // TODO: Handle this case.
+                        break;
+                      case HomeElement.social:
+                        // TODO: Handle this case.
+                        break;
+                    }
+                  },
+                  child: Column(
+                    children: [
+                      SvgPicture.asset(
+                        item.imagePath,
+                        height: 50,
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        item.title,
+                        textAlign: TextAlign.center,
+                      )
+                    ],
+                  ),
                 ),
               );
             }));
